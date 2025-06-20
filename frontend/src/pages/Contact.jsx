9baa9d655f92b54a08b936bc85ef5b33c661277c
suki-stark-cmd <sukielectrotech@gmail.com>
2025-06-20 10:12:52 +0530

    Text edited

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -13,8 +13,8 @@ const Contact = () => {
         <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className=' font-semibold text-lg text-gray-600'>OUR OFFICE</p>
-          <p className=' text-gray-500'>54709 Willms Station <br /> Suite 350, Washington, USA</p>
-          <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: medicoplus.tech@gmail.com</p>
+          <p className=' text-gray-500'>Othakalmandapam<br />641032-Coimbatore,TamilNadu,India</p>
+          <p className=' text-gray-500'>Contact: +91 8903137541 <br /> Email: medicoplus.tech@gmail.com</p>
           <p className=' font-semibold text-lg text-gray-600'>Careers at MEDICOPLUS</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
